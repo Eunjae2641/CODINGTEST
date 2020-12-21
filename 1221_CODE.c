69ab2e4d44b5f9b90d4a90691f8033d40d62e227
Shin Eun Jae <68882460+Eunjae2641@users.noreply.github.com>
2020-12-21 19:00:29 +0900

    Update 1221_CODE.c

--- 1221_CODE.c
+++ 1221_CODE.c
@@ -35,3 +35,18 @@ double solution(int arr[], size_t arr_len) {
     answer = sum / arr_len;
     return answer;
 }
+
+//짝수와 홀수
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+char* solution(int num) {
+        char* answer = (char*)malloc(sizeof(int));
+    if(num%2==0){
+        answer = "Even";
+    }
+    else
+        answer = "Odd";
+    return answer;
+}
