0727d2af3e27eec4cd49d87de094f4b82db4be76
Shin Eun Jae <68882460+Eunjae2641@users.noreply.github.com>
2020-12-21 18:30:59 +0900

    Create 1221_CODE.c

---
+++ 1221_CODE.c
@@ -0,0 +1,20 @@
+//두 정수 사이의 합
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    if(a<b){
+        for(int i = a; i<=b; i++){
+          answer += i;
+        }
+    }
+    else{
+        for(int i=b; i<=a; i++){
+            answer += i;
+        }
+     }
+    return answer;
+}
