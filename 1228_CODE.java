ad7fd93a4ba7a5cfa05941b051d1a84bc2a6eeac
Shin Eun Jae <68882460+Eunjae2641@users.noreply.github.com>
2020-12-28 23:14:13 +0900

    Create 1228_CODE.java

---
+++ 1228_CODE.java
@@ -0,0 +1,15 @@
+//약수의 합
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int i;
+        for(i=1; i<=n; i++){
+            if(n%i==0){
+                answer += i;
+                
+            }
+        }
+        return answer;
+    }
+
+}
